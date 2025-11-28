923b5f27aba257dc34007616697bff1d1508e764
Paigro <paigro9@gmail.com>
2025-11-28 12:00:07 +0100

    P6 a medias.

--- Practica6/BD-Paigro-6.sql
+++ Practica6/BD-Paigro-6.sql
@@ -1,2 +1,32 @@
 --Pablo Iglesias Rodrigo
 --Practica 6. PL-SQL
+
+DROP TABLE P6_PEDIDOSCLIENTE CASCADE CONSTRAINTS;
+CREATE TABLE P6_PEDIDOSCLIENTE(
+    customer_ID NUMBER(6), 
+    customer_first_name VARCHAR2(20),
+    customer_last_name VARCHAR2(20),
+    order_ID NUMBER(12), 
+    order_date TIMESTAMP(6),
+    order_status NUMBER(2),
+    order_total NUMBER(8, 2),
+    CONSTRAINT P6_PK_pedidos PRIMARY KEY (customer_ID),
+    CONSTRAINT P6_FK_pedidos_customerid FOREIGN KEY (customer_ID) 
+         REFERENCES PR4_customers(customer_ID),
+    CONSTRAINT P6_FK_pedidos_orderid FOREIGN KEY (order_id) 
+         REFERENCES PR4_orders(order_id)
+);
+
+CREATE PROCEDURE p6_proc_pedidos (customer_id P6_PEDIDOSCLIENTE.customer_id%TYPE) IS
+    
+    BEGIN 
+             
+    END;
+/ 
+
+
+
+
+
+
+
